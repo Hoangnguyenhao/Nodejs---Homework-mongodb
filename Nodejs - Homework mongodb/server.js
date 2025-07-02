14c66d4c8ceddf15f89166a9be58b8dec080ed22
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-07-02 15:39:52 +0700

    Update server.js

--- Nodejs - Homework mongodb/server.js
+++ Nodejs - Homework mongodb/server.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const productRoutes = require('./routes/productRoutes');
-const errorHandler = require('./middlewares/errorHandler');
+const app = require('./app');
+const connectDB = require('./config/db');
 
-dotenv.config();
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use('/api/products', productRoutes);
-app.use(errorHandler);
+connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Product Management API');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-module.exports = app;
